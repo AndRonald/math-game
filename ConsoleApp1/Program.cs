6d53751e33e0244dcad78e851fab8e3ca14150b0
Ronald Araujo <ronald.cortezlouy@gmail.com>
2026-04-08 23:08:06 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o seu nome: ");
+            string name = Console.ReadLine()!;
+            Console.WriteLine($"Bem vindo, Jogador: {name}");
+
+            int validacaoContinuaOuSai;
+            do
+            {
+                Painel();
+                validacaoContinuaOuSai = int.Parse(Console.ReadLine()!);
+                Console.WriteLine();
+                Random random = new Random();
+                
+            }
+            while (validacaoContinuaOuSai == 1);
+        }
+
+        public static void Painel()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("1 - CONTINUAR");
+            Console.WriteLine("0 - SAIR");
+        }
+
+    }
+}
