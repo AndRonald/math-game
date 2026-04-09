c7663e33244c731592995f056a8434e2ba18ab7a
Ronald Araujo <ronald.cortezlouy@gmail.com>
2026-04-09 00:02:54 -0300

    feat: inicia estrutura da entidade Calculadora
    - centraliza operações matemáticas
    - prepara base para abstração das perguntas
    
    TODO:
    - aplicar princípio DRY
    - avaliar abstração para diferentes operações

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Entities;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,21 +7,41 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-
-            Console.WriteLine("Digite o seu nome: ");
+            Random random = new Random();
+            Console.Write("Digite o seu nome: ");
             string name = Console.ReadLine()!;
+            Console.Clear();
             Console.WriteLine($"Bem vindo, Jogador: {name}");
+
 
             int validacaoContinuaOuSai;
             do
             {
+                int numberOne = random.Next(1, 100);
+                int numberTwo = random.Next(1, 100);
+
+                Calculadora.Somar(numberOne, numberTwo);
+                Console.Write($"{numberOne}+{numberTwo}: ");
+                int result = int.Parse(Console.ReadLine()!);
+
+                List<string> guardaAcertos = new List<string>();
+
+                if (result == Calculadora.Somar(numberOne, numberTwo))
+                    guardaAcertos.Add("Acertou a questão!");
+                else
+                    guardaAcertos.Add("Errou a questão!");
+
+                foreach (var item in guardaAcertos)
+                {
+                    Console.WriteLine(item);
+                }
                 Painel();
                 validacaoContinuaOuSai = int.Parse(Console.ReadLine()!);
-                Console.WriteLine();
-                Random random = new Random();
-                
             }
             while (validacaoContinuaOuSai == 1);
+
+
+
         }
 
         public static void Painel()
