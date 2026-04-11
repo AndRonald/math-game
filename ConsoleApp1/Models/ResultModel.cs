713bd5560be81f0d4ee40ccfee59d9b2e306d28c
Ronald Araujo <ronald.cortezlouy@gmail.com>
2026-04-11 19:17:04 -0300

    refactor: remove classe Calculadora e move lógica para Program (temporário) feat: adiciona lista de resultados das somas

---
+++ ConsoleApp1/Models/ResultModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace ConsoleApp1.Models
+{
+    public class ResultModel
+    {
+        public int Id { get; set; }
+        public int Corrects { get; set; }
+        public int Erros { get; set; }
+        public List<string> previousAccountSafekeeping { get; set; } = new List<string>();
+    }
+}
