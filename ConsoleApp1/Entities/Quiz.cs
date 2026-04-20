0c76422182fb1449b09c88251f022b1829e1a61d
Ronald Araujo <ronald.cortezlouy@gmail.com>
2026-04-20 02:05:06 -0300

    quiz creation:
    sum/subtraction/multiplication/division

---
+++ ConsoleApp1/Entities/Quiz.cs
@@ -0,0 +1,105 @@
+﻿using ConsoleApp1.Models;
+using System;
+
+namespace ConsoleApp1.Entities
+{
+    public class Quiz
+    {
+        public static Random random { get; set; } = new Random();
+        public static void QuizSoma(ResultModel modelSum) 
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                int firstNumber = random.Next(1, 100);
+                int secondNumber = random.Next(1, 100);
+                Console.Write($"{i}º) {firstNumber}+{secondNumber}= ");
+
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result == Operations.Addition(firstNumber, secondNumber))
+                        modelSum.Successes += 1;
+                    else
+                        modelSum.Errors += 1;
+
+                    modelSum.ListOfAnswers.Add($"{firstNumber} + {secondNumber}: {result}");
+                }
+                else
+                    Console.WriteLine("Invalid input");
+            }
+            Console.WriteLine($"\nCorrect: {modelSum.Successes}\nErrors: {modelSum.Errors}\n");
+        }
+
+        public static void QuizSubtraction(ResultModel modelSubtraction)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                int firstNumber = random.Next(1, 100);
+                int secondNumber = random.Next(1, 100);
+                Console.Write($"{i}º) {firstNumber}-{secondNumber}= ");
+
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result == Operations.Subtraction(firstNumber, secondNumber))
+                        modelSubtraction.Successes += 1;
+                    else
+                        modelSubtraction.Errors += 1;
+
+                    modelSubtraction.ListOfAnswers.Add($"{firstNumber} - {secondNumber}: {result}");
+                }
+                else
+                    Console.WriteLine("Invalid input");
+            }
+            Console.WriteLine($"\nCorrect: {modelSubtraction.Successes}\nErrors: {modelSubtraction.Errors}\n");
+        }
+
+        public static void QuizMultiplication(ResultModel modelMultiplication)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                int firstNumber = random.Next(1, 100);
+                int secondNumber = random.Next(1, 100);
+                Console.Write($"{i}º) {firstNumber}*{secondNumber}= ");
+
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result == Operations.Multiplication(firstNumber, secondNumber))
+                        modelMultiplication.Successes += 1;
+                    else
+                        modelMultiplication.Errors += 1;
+
+                    modelMultiplication.ListOfAnswers.Add($"{firstNumber} * {secondNumber}: {result}");
+                }
+                else
+                    Console.WriteLine("Invalid input");
+            }
+            Console.WriteLine($"\nCorrect: {modelMultiplication.Successes}\nErrors: {modelMultiplication.Errors}\n");
+        }
+
+        public static void QuizDivision(ResultModel modelDivision)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                int numberOne, numberTwo;
+                do
+                {
+                    numberOne = random.Next(1, 100);
+                    numberTwo = random.Next(1, 100);
+                }
+                while (numberOne % numberTwo != 0);
+                Console.Write($"{i}º) {numberOne} / {numberTwo}= ");
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result == Operations.Division(numberOne, numberTwo))
+                        modelDivision.Successes += 1;
+                    else
+                        modelDivision.Errors += 1;
+
+                    modelDivision.ListOfAnswers.Add($"{numberOne} / {numberTwo}: {result}");
+                }
+                else
+                    Console.WriteLine("Invalid input");
+            }
+            Console.WriteLine($"\nCorrect: {modelDivision.Successes}\nErrors: {modelDivision.Errors}\n");
+        }
+    }
+}
--- ConsoleApp1/Models/ResultModel.cs
+++ ConsoleApp1/Models/ResultModel.cs
@@ -4,9 +4,27 @@ namespace ConsoleApp1.Models
 {
     public class ResultModel
     {
-        public int Id { get; set; }
-        public int Corrects { get; set; }
-        public int Erros { get; set; }
-        public List<string> PreviousAccountSafekeeping { get; set; } = new List<string>();
+        public int Successes { get; set; }
+        public int Errors { get; set; }
+        public List<string> ListOfAnswers { get; set; } = new List<string>();
+
+
+        public static void ShowResult(ResultModel modelShow)
+        {
+            if (modelShow.ListOfAnswers.Count == 0)
+                Console.WriteLine("Não há registros");
+            else
+            {
+                for (int i = 0; i < modelShow.ListOfAnswers.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}º: {modelShow.ListOfAnswers[i]}");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Corrects: {Successes}, Errors: {Errors}, Previous Accounts: {string.Join(", ", ListOfAnswers)}";
+        }
     }
 }
